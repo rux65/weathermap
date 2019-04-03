de18de94deb26ea609a8df4c91b6dc6af57eb03f
Ruxi <ruxi_65@yahoo.com>
2019-04-03 20:35:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import json
+from flask import Flask , render_template, request
 
 endpoint = "http://api.openweathermap.org/data/2.5/weather"
 
@@ -8,9 +9,9 @@ payload = {"q": "London,UK", "units":"metric", "appid":"2bb229a44403af08980fc4be
 
 response = requests.get(endpoint, params=payload)
 
-print (response.url)
-print (response.status_code)
-print (response.headers["content-type"])
+#print (response.url)#
+#print (response.status_code)
+#print (response.headers["content-type"])
 print(response.text)
 
 ##200 means ok
@@ -18,9 +19,34 @@ print(response.text)
 ##304 - you hit thi api before and you are not having anything new
 
 
-print (json.loads(response.text)["coord"]["lat"])
+#print (json.loads(response.text)["coord"]["lat"])
 
-for weather in json.loads(response.text)["weather"]:
-	print(weather["main"])
+#for weather in json.loads(response.text)["weather"]:
+#	print(weather["main"])
 	
+
+
+
+
+app=Flask("myApp")
+	
+@app.route("/postcode")
+def hello_someone():
+	weather = json.loads(response.text)
+	coords = weather["coord"]
+	return render_template("weatherApi.html", weather=weather,coords=coords)
+
+email=[]
+
+@app.route("/signup", methods=["POST"])
+def signup():
+	form_data=request.form
+	var=form_data["email"]
+	#return "all ok"
+	return var ## returns one or the other 
+	print (var)
+app.run(debug=True) # always run with debug true 
+
+
+
 	
