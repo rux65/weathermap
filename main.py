d51014e889a4017df48c28e8542e96b819efbe2a
Ruxi <ruxi_65@yahoo.com>
2019-04-04 23:59:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def meh():
 
 	
 endpoint = "http://api.openweathermap.org/data/2.5/weather"
-
+endpoint2 = "http://api.openweathermap.org/data/2.5/weather"
 	#print (response.url)#
 	#print (response.status_code)
 	#print (response.headers["content-type"])
@@ -29,13 +29,20 @@ endpoint = "http://api.openweathermap.org/data/2.5/weather"
 def weather_reply():
 	form_data=request.form
 	var=form_data["text"]
-	payload = {"q":var, "units":"metric", "appid":"2bb229a44403af08980fc4be234f94a9"}
-	response = requests.get(endpoint, params=payload)
+	payload = {"q":var, "units":"metric", "appid":"2bb229a44403af08980fc4be234f94a9"} 
+	response = requests.get(endpoint, params=payload, data={"key":"value"})
 	api = json.loads(response.text)
 	coord = [api["coord"]]
 	weather= api["weather"]
-	print(response.text)
-	return render_template("location.html", api=api,coord=coord, weather=weather)
+	#print (api["coord"]["lon"]+1)
+	
+	
+	r=requests.get("http://api.openweathermap.org/data/2.5/weather?lat=%f&lon=%f" %(api['coord']['lat']+0.2, api['coord']['lon']+0.2))
+	print(r.text)
+	
+	#print(response.text)
+	return render_template("location2.html", api=api,coord=coord, weather=weather)
 
+	
 app.run(debug=True) # always run with debug true 
 
